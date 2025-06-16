a265090327b19162253a591727c9d6b0a5f8d4e7
Yifu Ding <yifuding.twp@gmail.com>
2025-06-16 19:37:52 -0400

    updated ns.scp() syntax

--- src/Orchestrator/Boot.ts
+++ src/Orchestrator/Boot.ts
@@ -22,17 +22,17 @@ export async function main(ns: NS) {
     if (option === "no-server-manager") scriptList = scriptList.filter(s => s !== ChannelName.serverManager)
 
     if (MANAGING_SERVER !== "home") {
-        await ns.scp(ns.ls("home", BASE_DIR), "home", MANAGING_SERVER)
+        await ns.scp(ns.ls("home", BASE_DIR), MANAGING_SERVER, "home")
         ns.tprint("Copying " + ns.ls("home", BASE_DIR).length + " files to " + MANAGING_SERVER)
     }
 
     if (HACKING_SERVER !== "home") {
-        await ns.scp(ns.ls("home", BASE_DIR), "home", HACKING_SERVER)
+        await ns.scp(ns.ls("home", BASE_DIR), HACKING_SERVER, "home")
         ns.tprint("Copying " + ns.ls("home", BASE_DIR).length + " files to " + HACKING_SERVER)
     }
 
     if (THREAD_SERVER !== "home") {
-        await ns.scp(ns.ls("home", BASE_DIR), "home", THREAD_SERVER)
+        await ns.scp(ns.ls("home", BASE_DIR), THREAD_SERVER, "home")
         ns.tprint("Copying " + ns.ls("home", BASE_DIR).length + " files to " + THREAD_SERVER)
     }
 
--- src/Orchestrator/Common/GenericFunctions.ts
+++ src/Orchestrator/Common/GenericFunctions.ts
@@ -11,7 +11,7 @@ export async function copyFile(ns: NS, fileList: string[], host) {
     for (let j = 0; j < fileList.length; j++) {
         const script: string = fileList[j]
         ns.fileExists(script, host) && ns.rm(script, host)
-        await ns.scp(script, "home", host);
+        await ns.scp(script, host, "home");
     }
 }
 
